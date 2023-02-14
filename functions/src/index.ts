94e00df5a25e57ea5473c870598017302daf0735
Dominik Müller <dominik@mueteam.de>
2023-02-14 23:07:00 +0100

    feat: converted fb function to proper typescript

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,27 +1,29 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-export const memberJoin = functions.https.onCall(async (data, context) => {
-  const { userId, teamid, tokenid } = data;
+export const memberJoin = functions.https.onCall(
+  async (data: any, context: functions.https.CallableContext) => {
+    const { userId, teamid, tokenid } = data;
 
-  // Check that the user is authenticated
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
-      "unauthenticated",
-      "You must be logged in to join a team."
-    );
+    // Check that the user is authenticated
+    if (!context.auth) {
+      throw new functions.https.HttpsError(
+        "unauthenticated",
+        "You must be logged in to join a team."
+      );
+    }
+
+    // Add the user to the team in the "members" collection
+    const membersRef = admin
+      .firestore()
+      .collection("teams")
+      .doc(teamid)
+      .collection("members");
+    await membersRef.doc(userId).set({ joinedAt: new Date() });
+
+    // TODO: Implement token verification and deletion
+
+    // Return a success message
+    return { message: `User ${userId} joined team ${teamid}.` };
   }
-
-  // Add the user to the team in the "members" collection
-  const membersRef = admin
-    .firestore()
-    .collection("teams")
-    .doc(teamid)
-    .collection("members");
-  await membersRef.doc(userId).set({ joinedAt: new Date() });
-
-  // TODO: Implement token verification and deletion
-
-  // Return a success message
-  return { message: `User ${userId} joined team ${teamid}.` };
-});
+);
