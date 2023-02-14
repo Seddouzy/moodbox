d633cb699a0f81c0ecdc7885ae2afbab8514072d
Dominik Müller <dominik@mueteam.de>
2023-02-14 23:48:03 +0100

    fix: reformat fix for fb functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import * as admin from "firebase-admin";
 
 export const memberJoin = functions.https.onCall(
   async (data: any, context: functions.https.CallableContext) => {
-    const { userId, teamid, tokenid } = data;
+    const { userId, teamId } = data;
 
     // Check that the user is authenticated
     if (!context.auth) {
@@ -17,13 +17,13 @@ export const memberJoin = functions.https.onCall(
     const membersRef = admin
       .firestore()
       .collection("teams")
-      .doc(teamid)
+      .doc(teamId)
       .collection("members");
     await membersRef.doc(userId).set({ joinedAt: new Date() });
 
     // TODO: Implement token verification and deletion
 
     // Return a success message
-    return { message: `User ${userId} joined team ${teamid}.` };
+    return { message: `User ${userId} joined team ${teamId}.` };
   }
 );
