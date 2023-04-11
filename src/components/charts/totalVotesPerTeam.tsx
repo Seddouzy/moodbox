37b8d6be7026228e75f5cf4ff6d5bca90cc590cf
zeddouzy <david.seddaoui@icloud.com>
2023-04-11 12:01:03 +0200

    FEATURE: totalvotesperTeam

---
+++ src/components/charts/totalVotesPerTeam.tsx
@@ -0,0 +1,11 @@
+import { ComponentType } from "react";
+
+interface TotalVotesPerTeamProps {
+  sentiments: { sentimentAvg: number; date: Date }[];
+}
+
+const TotalVotesPerTeam: ComponentType<TotalVotesPerTeamProps> = ({}) => {
+  return <></>;
+};
+
+export default TotalVotesPerTeam;
