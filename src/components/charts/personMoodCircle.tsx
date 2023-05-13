c07282b9cadcf55836645056fbc74399a4e115cc
zeddouzy <david.seddaoui@icloud.com>
2023-05-13 13:22:45 -0400

    FEAT: personSentimentbyDay finished and changed

--- src/components/charts/personMoodCircle.tsx
+++ src/components/charts/personMoodCircle.tsx
@@ -1,6 +1,10 @@
 import { AnonymousVote } from "@/shared/interface/AnonymousVote";
 import { pickFromGradient } from "@/shared/services/colorService";
-import { HeartIcon, UserGroupIcon } from "@heroicons/react/24/outline";
+import {
+  HeartIcon,
+  UserGroupIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 import {
   collection,
   getDocs,
@@ -55,7 +59,7 @@ const PersonMoodCircle: ComponentType<PersonMoodCircleProps> = ({
         }}
         className="text-black/50 flex flex-row gap-2 items-center outline outline-2 outline-offset-4 outline-transparent bg-gradient-to-br px-4 py-2 rounded-lg uppercase"
       >
-        <UserGroupIcon className="w-6 h-6" />
+        <UserIcon className="w-6 h-6" />
         <HeartIcon className="w-6 h-6" />
         {Math.round(numAvgVotes * 100) / 100}
       </div>
